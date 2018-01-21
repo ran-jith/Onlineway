e5f8a8b002dcb6d972707f774f872d2b5f0b48ad
Ranjith R <ranjithakalurkk@gmail.com>
2018-01-21 19:27:00 +0530

    Create config.php

---
+++ register/homef/web/config.php
@@ -0,0 +1,15 @@
+<?php
+$con=mysql_connect("localhost","root","password") or die("connection error") ;
+mysql_select_db('dbms') or die("db error");
+
+if($con==true)
+{
+    echo "Success";
+}
+else
+{
+    mysql_close($con);
+}
+?>
+
+
